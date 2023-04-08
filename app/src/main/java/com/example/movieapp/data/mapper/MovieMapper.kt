a2ef93090e63bae63540430bbac5f7eb542c92d8
Delma <delmant1985@gmail.com>
2023-04-08 15:46:11 +0300

    added mapper and MapStruct dependency

--- app/src/main/java/com/example/movieapp/data/mapper/MovieMapper.kt
+++ app/src/main/java/com/example/movieapp/data/mapper/MovieMapper.kt
@@ -1,6 +1,10 @@
 package com.example.movieapp.data.mapper
 
+import Movie
+import com.example.movieapp.data.network.model.MovieDto
 import javax.inject.Inject
 
 class MovieMapper @Inject constructor() {
+
+
 }
