99325b39014a121bd8b7883d5e088583c87b9b1d
Delma <delmant1985@gmail.com>
2023-04-13 11:37:15 +0300

    add binding in MovieDetailFragment

--- app/src/main/java/com/example/movieapp/presentation/movie_detail_fragment/MovieDetailFragment.kt
+++ app/src/main/java/com/example/movieapp/presentation/movie_detail_fragment/MovieDetailFragment.kt
@@ -1,12 +1,32 @@
 package com.example.movieapp.presentation.movie_detail_fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.movieapp.databinding.FragmentMovieDetailInfoBinding
 import com.example.movieapp.domain.model.Movie
 
 class MovieDetailFragment: Fragment() {
 
+    private var _binding: FragmentMovieDetailInfoBinding? = null
+    private val binding: FragmentMovieDetailInfoBinding
+        get() = _binding ?: throw RuntimeException("FragmentMovieDetailInfoBinding is null")
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMovieDetailInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
     companion object {
 
