c8bee6d249b44c9eb2a0be0c24365264e33ed202
Delma <delmant1985@gmail.com>
2023-04-13 11:51:34 +0300

    add inject MovieDetailFragment method in component

--- app/src/main/java/com/example/movieapp/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/movieapp/di/ApplicationComponent.kt
@@ -1,6 +1,7 @@
 package com.example.movieapp.di
 
 import android.app.Application
+import com.example.movieapp.presentation.movie_detail_fragment.MovieDetailFragment
 import dagger.BindsInstance
 import dagger.Component
 
@@ -8,7 +9,7 @@ import dagger.Component
 @Component(modules = [DataModule::class, ViewModelModule::class])
 interface ApplicationComponent {
 
-
+    fun inject(fragment: MovieDetailFragment)
 
     @Component.Factory
     interface ApplicationComponentFactory {
