15380d73198c555174c7d90424206509a9faecd4
Delma <delmant1985@gmail.com>
2023-04-15 11:20:57 +0300

    added bottom navigation bar in xml and create base for navigation in main activity. also added three icons for menu

--- app/src/main/java/com/example/movieapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/presentation/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Window
 import com.bumptech.glide.Glide
+import com.example.movieapp.R
 import com.example.movieapp.databinding.ActivityMainBinding
 import com.example.movieapp.data.network.ApiFactory
+import com.example.movieapp.di.ApplicationComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
@@ -19,53 +21,24 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    private val apiService = ApiFactory.apiService
 
-    private val coroutineScopeDefault = CoroutineScope(Dispatchers.Default)
-    private val coroutineScopeMain = CoroutineScope(Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
+        supportFragmentManager.beginTransaction()
 
-        binding.btn.setOnClickListener {
-            coroutineScopeDefault.launch {
-//                val movie1 = apiService.getMovieByPage(1,1)
-//                Log.d("MOVIE", movie1.toString())
-                val movie2 = apiService.getRandomMovie()
-                Log.d("MOVIE", movie2.toString())
-                coroutineScopeMain.launch {
-                    binding.tvText.text = movie2.name
-                    Glide.with(this@MainActivity)
-                        .load("https://st.kp.yandex.net/images/film_big/${movie2    .id}.jpg")
-                        .into(binding.ivMovieImg)
+        binding.bottomNavigationView.setOnItemReselectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+
+                }
+                R.id.favourite -> {
+
+                }
+                R.id.search -> {
 
                 }
             }
         }
-//        val movieList = mutableListOf<MovieDto>()
-//        coroutineScopeDefault.launch {
-//            repeat(10) {
-//                val movie = apiService.getRandomMovie()
-//                movieList.add(movie)
-//                Log.d("MOVIE", movie.toString())
-//            }
-//            coroutineScopeMain.launch {
-//                setupRv(movieList)
-//            }
-//        }
-//
-    }
-
-//    private fun setupRv(list: List<MovieDto>) {
-//        val adapter = MovieAdapter(this)
-//        binding.rvMovies.adapter = adapter
-//        binding.rvMovies.layoutManager = GridLayoutManager(this ,4)
-//        adapter.submitList(list)
-//    }
-    override fun onDestroy() {
-        super.onDestroy()
-        coroutineScopeDefault.cancel()
-        coroutineScopeMain.cancel()
     }
 }
