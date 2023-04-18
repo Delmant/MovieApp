16899bc7c12c21220cec123ab9c7fc3c48e67bc2
Delma <delmant1985@gmail.com>
2023-04-18 17:21:00 +0300

    added actor adapter with viewholder

---
+++ app/src/main/java/com/example/movieapp/presentation/movie_detail_fragment/actor/ActorViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.movieapp.presentation.movie_detail_fragment.actor
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movieapp.databinding.ActorItemBinding
+
+class ActorViewHolder(
+    val binding: ActorItemBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+}
