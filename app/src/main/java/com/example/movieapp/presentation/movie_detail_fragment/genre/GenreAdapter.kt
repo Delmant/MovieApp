9e0fbd158c91f3e7d965a7f5a7b69b7b4f4bb93f
Delma <delmant1985@gmail.com>
2023-04-18 12:35:33 +0300

    added genre adapter with viewholder

---
+++ app/src/main/java/com/example/movieapp/presentation/movie_detail_fragment/genre/GenreAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.movieapp.presentation.movie_detail_fragment.genre
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movieapp.databinding.GenreItemBinding
+import com.example.movieapp.domain.model.Genres
+
+class GenreAdapter(
+    private val items: List<Genres>
+    ): RecyclerView.Adapter<GenreViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenreViewHolder {
+        val binding = GenreItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return GenreViewHolder((binding))
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: GenreViewHolder, position: Int) {
+       val item = items[position]
+        with(holder.binding) {
+            with(item) {
+                genre.text = item.name
+            }
+        }
+    }
+}
