206f398993fafb1f48f9f9d0b00161b8390039ab
Delma <delmant1985@gmail.com>
2023-04-28 09:41:56 +0300

    added a layout for detailed information about the facts and edited the adapter

--- app/src/main/java/com/example/movieapp/data/network/model/actor_dto/FactDto.kt
+++ app/src/main/java/com/example/movieapp/data/network/model/actor_dto/FactDto.kt
@@ -1,13 +1,13 @@
 package com.example.movieapp.data.network.model.actor_dto
 
-import com.example.movieapp.domain.model.actor.Fact
+import com.example.movieapp.domain.model.actor.ActorFact
 
 data class FactDto(
     val value: String?
 ) {
     companion object {
-        fun toEntity(factDto: FactDto): Fact {
-            return Fact(
+        fun toEntity(factDto: FactDto): ActorFact {
+            return ActorFact(
                 value = factDto.value ?: ""
             )
         }
--- app/src/main/java/com/example/movieapp/presentation/movie_detail_fragment/fact/FactAdapter.kt
+++ app/src/main/java/com/example/movieapp/presentation/movie_detail_fragment/fact/FactAdapter.kt
@@ -5,13 +5,14 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movieapp.databinding.FactsItemBinding
 import com.example.movieapp.domain.model.movie.Facts
+import com.example.movieapp.presentation.movie_detail_fragment.actor.ActorAdapter
 
 
 class FactAdapter(
     private val items: List<Facts>
 ) : RecyclerView.Adapter<FactViewHolder>() {
 
-
+    var listener: FactAdapter.OnItemClickListener? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FactViewHolder {
         val binding = FactsItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return FactViewHolder((binding))
@@ -27,6 +28,13 @@ class FactAdapter(
             with(item) {
                 tvDesc.text = item.value
             }
+            root.setOnClickListener {
+                listener?.onItemClick(item.value)
+            }
         }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(fact: String)
     }
 }
