5a1b2ff7b7606051d6a0f16aaf3a70d0aa804651
Delma <delmant1985@gmail.com>
2023-04-28 09:39:01 +0300

    edited the actor adapter. now an action takes place on a click

---
+++ app/src/main/java/com/example/movieapp/presentation/actor_detail_fragment/actor_movies/ActorMoviesViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.movieapp.presentation.actor_detail_fragment.actor_movies
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movieapp.databinding.ActorMoviesItemBinding
+
+class ActorMoviesViewHolder(val binding: ActorMoviesItemBinding): RecyclerView.ViewHolder(binding.root) {
+}
