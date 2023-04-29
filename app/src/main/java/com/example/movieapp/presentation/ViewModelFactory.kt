91c6bd65280b3541652ec574dcb2fac3dca6ec95
Delma <delmant1985@gmail.com>
2023-04-29 08:43:54 +0300

    fixed bugs in the view model factory

--- app/src/main/java/com/example/movieapp/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/movieapp/presentation/ViewModelFactory.kt
@@ -6,10 +6,10 @@ import javax.inject.Inject
 import javax.inject.Provider
 
 class ViewModelFactory @Inject constructor(
-    private val viewModelProvider: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
-): ViewModelProvider.Factory {
+    private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
+) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return viewModelProvider[modelClass]?.get() as T
+        return viewModelProviders[modelClass]?.get() as T
     }
 }
