83fced5516344c2840740af7657405261f26287c
Delma <delmant1985@gmail.com>
2023-05-16 14:16:16 +0300

    added an adapter for search elements

---
+++ app/src/main/java/com/example/movieapp/presentation/adapters/search/SearchViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.movieapp.presentation.adapters.search
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movieapp.databinding.SearchItemBinding
+
+class SearchViewHolder(val binding: SearchItemBinding): RecyclerView.ViewHolder(binding.root) {
+}
