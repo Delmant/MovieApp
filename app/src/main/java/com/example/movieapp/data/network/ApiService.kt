55b5f41dcdaaf2c84622d7898884085400abafc2
Delma <delmant1985@gmail.com>
2023-05-28 10:30:15 +0300

    completed the method of getting the settings

--- app/src/main/java/com/example/movieapp/data/network/ApiService.kt
+++ app/src/main/java/com/example/movieapp/data/network/ApiService.kt
@@ -81,6 +81,10 @@ interface ApiService {
     ): ReviewListDto
 
     @GET("v1/movie/possible-values-by-field")
+    @Headers(
+        MEDIA_TYPE,
+        API_KEY
+    )
     suspend fun getSettingsValue(
         @Query(SETTINGS_PARAM_VALUE) field: String
     ): List<SettingsValueDto>
