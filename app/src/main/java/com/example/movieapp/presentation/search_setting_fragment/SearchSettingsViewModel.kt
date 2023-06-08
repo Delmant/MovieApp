1fd1cf1c66f7b2e088f97138335d4e33fa806580
Delma <delmant1985@gmail.com>
2023-06-08 12:25:21 +0300

    added work with saving settings from two fragments

--- app/src/main/java/com/example/movieapp/presentation/search_setting_fragment/SearchSettingsViewModel.kt
+++ app/src/main/java/com/example/movieapp/presentation/search_setting_fragment/SearchSettingsViewModel.kt
@@ -1,7 +1,24 @@
 package com.example.movieapp.presentation.search_setting_fragment
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.movieapp.domain.repository.SearchSettingsRepository
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class SearchSettingsViewModel @Inject constructor() : ViewModel() {
+class SearchSettingsViewModel @Inject constructor(
+    private val searchSettingsRepository: SearchSettingsRepository
+) : ViewModel() {
+
+    fun saveSearchSettings(type: String, json: String) {
+        viewModelScope.launch {
+            searchSettingsRepository.saveSearchSettings(type, json)
+        }
+    }
+
+    fun getSearchSettings() {
+        viewModelScope.launch {
+            searchSettingsRepository.getSearchSettings()
+        }
+    }
 }
--- app/src/main/java/com/example/movieapp/presentation/search_settings_detail_fragment/SearchSettingsDetailViewModel.kt
+++ app/src/main/java/com/example/movieapp/presentation/search_settings_detail_fragment/SearchSettingsDetailViewModel.kt
@@ -5,14 +5,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.movieapp.domain.model.search_settings.SettingsValue
+import com.example.movieapp.domain.repository.SearchSettingsRepository
 import com.example.movieapp.domain.usecases.GetSettingsValueUseCase
+import com.google.gson.Gson
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class SearchSettingsDetailViewModel @Inject constructor(
-    private val getSettingsValueUseCase: GetSettingsValueUseCase
-): ViewModel() {
+    private val getSettingsValueUseCase: GetSettingsValueUseCase,
+    private val searchSettingsRepository: SearchSettingsRepository
+) : ViewModel() {
 
+    private val gson = Gson()
     private var _liveData = MutableLiveData<List<SettingsValue>>()
     val liveData: LiveData<List<SettingsValue>> = _liveData
 
@@ -20,6 +24,12 @@ class SearchSettingsDetailViewModel @Inject constructor(
         viewModelScope.launch {
             val settingsValue = getSettingsValueUseCase.invoke(type)
             _liveData.value = settingsValue
+        }
+    }
+
+    fun saveSettings(type: String, list2: List<SettingsValue>) {
+        viewModelScope.launch {
+            searchSettingsRepository.saveSearchSettings(type, gson.toJson(list2))
         }
     }
 }
